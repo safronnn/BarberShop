96c53fc660e2180d95c11abeb2c1b38bf1358e0d
Hillar1ous <semen.zabirohin@yandex.ru>
2022-12-28 04:43:44 +0300

    Add serviceList.h

---
+++ servicelist.h
@@ -0,0 +1,24 @@
+#ifndef SERVICELIST
+#define SERVICELIST
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+// Класс ServiceList(Список предоставленных услуг)--------------------------------------------------------------
+class ServiceList{
+private:
+    list <Service*> SetPtrsServs;
+    list <Service*>::iterator iter;
+
+public:
+    ~ServiceList();
+    void insertService(Service*);
+    void displayService();
+    float getSum();
+};
+// Конец класса ServiceList-------------------------------------------------------------------------------------
+#endif // SERVICELIST
+
